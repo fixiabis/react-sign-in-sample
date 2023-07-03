69900e5c91af35867609e08ca1a42b2d3602e9d3
fixiabis <fixiabis@gmail.com>
2023-07-03 17:50:15 +0800

    feat(sign-in/form): active button & error input style

--- src/components/sign-in-form.tsx
+++ src/components/sign-in-form.tsx
@@ -52,15 +52,32 @@ export function SignInForm() {
 				</button>
 			</div>
 			<div className="sign-in-form--input -email -wrapper">
-				<input className="sign-in-form--input -email" type="email" placeholder="Email" />
-				{/* <div className="sign-in-form--input-popover">
-					<strong>Email not recognized.</strong>
-					<br />
-					Do you need to create an account?{" "}
-					<a className="sign-in-form--link" href="#">
-						Click here
-					</a>
-				</div> */}
+				<input className="sign-in-form--input -email -error" type="email" placeholder="Email" />
+				<div className="sign-in-form--input-popover">
+					<svg
+						className="sign-in-form--input-popover-background-image"
+						width="212"
+						height="80"
+						viewBox="0 0 212 80"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							fillRule="evenodd"
+							clipRule="evenodd"
+							d="m 29.9705 0 c -8.8366 0 -16 7.1634 -16 16 v 7.0001 l -11.3136 11.3136 c -3.1242 3.1242 -3.1242 8.1895 0 11.3137 l 11.3136 11.3136 v 7.059 c 0 8.8366 7.1634 16 16 16 h 165.9995 c 8.837 0 16 -7.1634 16 -16 v -48 c 0 -8.8366 -7.163 -16 -16 -16 h -165.9995 z"
+							fill="#9574CD"
+						/>
+					</svg>
+					<div className="sign-in-form--input-popover-content">
+						<strong>Email not recognized.</strong>
+						<br />
+						Do you need to create an account?{" "}
+						<a className="sign-in-form--link" href="#">
+							Click here
+						</a>
+					</div>
+				</div>
 			</div>
 			<div className="sign-in-form--input -password -wrapper">
 				<input className="sign-in-form--input -password" type="password" placeholder="Password" />
